6a2eb153825fa613600f16aa1d6b04054abaa463
Messyji <jzh@Mess.Mess-domain>
2013-01-13 22:27:18 +0800

    an demo

---
+++ bitmap/SPFA.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+	var spfa = function(){
+		var VERTEXNUM = 5;
+		var QUEUELEN = 10000;
+		var INF = 999999999.0;
+		var pre[VERTEXNUM];
+		var q[QUEUELEN];
+		var l, r;
+		
+		var d[VERTEXNUM];
+		for(i=0; i<n; i++){
+			d[i] = INF;
+		}
+	}
+	spfa();
+</script>
